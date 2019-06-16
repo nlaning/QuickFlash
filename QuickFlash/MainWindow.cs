e672d4ba59b2a382bafab7f817521b2e3cc4333a
Nathan Laning <nlaning@uvm.edu>
2019-06-16 15:59:09 -0400

    forgot to update version and dist.

--- QuickFlash/MainWindow.cs
+++ QuickFlash/MainWindow.cs
@@ -44,7 +44,7 @@ namespace QuickFlash
         long limit = 200000000;
         private BackgroundWorker checkDrivesWorker,flashDrivesWorker, magicDrivesWorker;
         string preferencesFile = "pref.txt"; //just incase a name change is necessary
-        string version = "1.21"; //probably should be replaced for proper versioning...
+        string version = "1.31"; //probably should be replaced for proper versioning...
         /* Main Window
         * first opening the program, (like main)
         * */
